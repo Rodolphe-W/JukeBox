9111e10d3c5125edae39ac026d29a60e96b7ba53
unknown <wallerand.rodolphe62@gmail.com>
2024-04-23 10:54:54 +0200

    fetch json

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,52 +1,32 @@
 const playlist = document.getElementById("playlist");
 const lecteur = document.querySelector("#lecteur");
-
-
-
 const config = {
     urlCover : "uploads/cover/",
     urlSound : "uploads/musics/"
-}
+};
 
-const dbMusics = [
-    {
-        id: 1,
-        cover: "1.jpg",
-        sound: "1.mp3",
-        title: "Titre 1",
-        category: "pop",
-    },
-    {
-        id: 2,
-        cover: "2.jpg",
-        sound: "2.mp3",
-        title: "Titre 2",
-        category: "pop",
-    },
-    {
-        id: 3,
-        cover: "3.jpg",
-        sound: "3.mp3",
-        title: "Titre 3",
-        category: "pop",
-    }
-]
+const getData = async () => {
+    const req = await fetch("./assets/js/data.json");
+    const dbMusics = await req.json();
+    console.log(dbMusics);
+    dbMusics.forEach((music) => {
+        playlist.innerHTML += `<li id="${music.id}"><h2>${music.title}</h2><img src="${config.urlCover}${music.cover}" alt="${music.title}"/><div><small>${music.category}</small></div></li>`;
+    });
 
-dbMusics.forEach((music) => {
-    playlist.innerHTML += `<li id="${music.id}"><h2>${music.title}</h2><img src="${config.urlCover}${music.cover}" alt="${music.title}"/><div><small>${music.category}</small></div></li>`;
-});
+    const allLi = document.querySelectorAll("li");
 
-const allLi = document.querySelectorAll("li");
+    allLi.forEach((li) => {
+        li.addEventListener('click', function(){
+            const id = parseInt(li.id);
+            const searchById = dbMusics.find((element) => element.id === id);
+            alert(`Veux-tu écouter le titre : ${searchById.title}`);
+            lecteur.src = `${config.urlSound}${searchById.sound}`;
+            lecteur.play();
+        })
+    });
+};
+getData();
 
-allLi.forEach((li) => {
-    li.addEventListener('click', function(){
-        const id = parseInt(li.id);
-        const searchById = dbMusics.find((element) => element.id === id);
-        alert(`Veux-tu écouter le titre : ${searchById.title}`);
-        lecteur.src = `${config.urlSound}${searchById.sound}`;
-        lecteur.play();
-    })
-})
 
 // fonction à mettre dans le onclick des li de la boucle foreach
 // function loadMusic(idMusic){
